6c7d6f4afcf37e324ac595e716e4f9ee74436b68
pro <pro@WINDOWS-PRO>
2026-06-01 18:21:24 +0300

    Выполнение 1 задания 2 лабораторной работы

--- src/Laba2/Study.LabWork2/Feature/Task1/SubTask1/SemaphoreService.cs
+++ src/Laba2/Study.LabWork2/Feature/Task1/SubTask1/SemaphoreService.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Study.LabWork2.Abstractions.Feature.Task1.SubTask1;
 using Study.LabWork2.Abstractions.Feature.Task1.SubTask1.DtoModels;
 
@@ -8,7 +9,80 @@ namespace Study.LabWork2.Feature.Task1.SubTask1;
 /// </summary>
 public sealed class SemaphoreService : IPrimeCounter
 {
-    public PrimeCountResultDto CountPrimes(int start, int end, int threadCount) => throw new NotImplementedException();
+    public PrimeCountResultDto CountPrimes(int start, int end, int threadCount)
+    {
+        if (start > end) throw new ArgumentException("start must be <= end");
+        if (threadCount <= 0) throw new ArgumentException("threadCount must be positive");
 
-    public string GetVersionName() => throw new NotImplementedException();
+        int primeCount = 0;
+        var foundPrimes = new List<int>();
+        using var semaphore = new Semaphore(1, 1);
+
+        var threads = CreateThreads(start, end, threadCount, (rangeStart, rangeEnd, threadId) =>
+        {
+            for (int num = rangeStart; num <= rangeEnd; num++)
+            {
+                Console.WriteLine($"Поток {threadId}: проверяю число {num}");
+
+                if (PrimeChecker.IsPrime(num))
+                {
+                    Console.WriteLine($"Поток {threadId}: число {num} ПРОСТОЕ");
+                    semaphore.WaitOne();
+                    try
+                    {
+                        primeCount++;
+                        foundPrimes.Add(num);
+                    }
+                    finally
+                    {
+                        semaphore.Release();
+                    }
+                }
+            }
+        });
+
+        var sw = Stopwatch.StartNew();
+        StartAndJoinThreads(threads);
+        sw.Stop();
+
+        return new PrimeCountResultDto
+        {
+            PrimeCount = primeCount,
+            ExecutionTime = sw.Elapsed,
+            ThreadCount = threadCount,
+            SynchronizationType = GetVersionName(),
+            FoundPrimes = foundPrimes
+        };
+    }
+
+    public string GetVersionName() => "Semaphore";
+
+    private static Thread[] CreateThreads(int start, int end, int threadCount, Action<int, int, int> action)
+    {
+        var threads = new Thread[threadCount];
+        int totalNumbers = end - start + 1;
+        int numbersPerThread = totalNumbers / threadCount;
+        int remainder = totalNumbers % threadCount;
+
+        int currentStart = start;
+        for (int i = 0; i < threadCount; i++)
+        {
+            int threadStart = currentStart;
+            int threadEnd = currentStart + numbersPerThread - 1;
+            if (i == threadCount - 1)
+                threadEnd += remainder;
+
+            int threadId = i + 1;
+            threads[i] = new Thread(() => action(threadStart, threadEnd, threadId));
+
+            currentStart = threadEnd + 1;
+        }
+        return threads;
+    }
+
+    private static void StartAndJoinThreads(Thread[] threads)
+    {
+        foreach (var t in threads) t.Start();
+        foreach (var t in threads) t.Join();
+    }
 }
